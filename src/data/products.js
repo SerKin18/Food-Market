2d13d4af075cd93533921a121994bab2c91de83d
Sergey <sergeykinyak@gmail.com>
2023-09-15 16:30:08 +0300

    food shop add parcel

--- src/data/products.js
+++ src/data/products.js
@@ -10,6 +10,7 @@ import img9 from '../i/im9.jpg'
 import img10 from '../i/im10.jpg'
 import img11 from '../i/im11.jpg'
 import img12 from '../i/im12.jpg'
+
 export const data = [
 	{ name: 'Овсяная каша с фруктами', img: img1, price: 25, food: 'breakfast', id: 1 },
 	{ name: 'Яичница глазунья с овощами на сковородке', img: img2, price: 25, food: 'breakfast', id: 2 },
@@ -20,7 +21,7 @@ export const data = [
 	{ name: 'Суп Пити', img: img7, price: 85, food: 'first-course', id: 7 },
 	{ name: 'Борщ украинский', img: img8, price: 95, food: 'first-course', id: 8 },
 	{ name: 'Суп Кюфта Бозбаш', img: img9, price: 100, food: 'first-course', id: 9 },
-	{ name: 'Картофель фри', img: img10, price: 125, food: 'garnish', id: 10 },
+	{ name: 'Картофель фри', img:img10, price: 125, food: 'garnish', id: 10 },
 	{ name: 'Картофель по-домашнему', img: img11, price: 135, food: 'garnish', id: 11 },
 	{ name: 'Рис с овощами', img: img12, price: 150, food: 'garnish', id: 12 },
 
